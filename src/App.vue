df19bdbbf170e61233270c721d7964678be1f6ea
raihan4816 <raihan4816@gmail.com>
2022-05-18 21:52:51 +0700

    add router-view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <div class="wrapper news-layout">
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
 
 <style>
 #app {
