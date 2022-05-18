40f07bc51f183e32d8c9cc620af6e68006497141
raihan4816 <raihan4816@gmail.com>
2022-05-18 21:57:40 +0700

    add Komponen FAQ

---
+++ src/components/FAQComp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="card text-bg-warning" style="width: 100%">
+      <div class="card-body">
+        <h5 class="card-title text-center fs-1">Contact Us</h5>
+        <div class="text-center">
+          <img
+            src="../assets/foto.jpeg"
+            class="rounded"
+            alt="..."
+            style="width: 20%"
+          />
+        </div>
+        <h5 class="card-title text-center">Raihan Hafizh</h5>
+        <h5 class="card-title text-center">English Education Program</h5>
+        <h5 class="card-title text-center">Institut Pendidikan Indonesia</h5>
+        <h5 class="card-title text-center">
+          Phone Number: +6287725051205 (Indonesia)
+        </h5>
+        <h5 class="card-title text-center">Instagram</h5>
+        <h5 class="card-title text-center">LinkedIn</h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FAQComp",
+};
+</script>
