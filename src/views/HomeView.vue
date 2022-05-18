dc30774ebed08a7af18a33bd911fd5c2560b8dd2
raihan4816 <raihan4816@gmail.com>
2022-05-18 22:03:03 +0700

    add HomePage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <h1>Welcome To SelfyBooks</h1>
+    </div>
+    <div>
+      <p class="text-lowercase fs-6">
+        where all people can share thoughts on Self Improvement Books
+      </p>
+    </div>
+    <HomeItem
+      v-for="(data, index) in HomeView"
+      :key="index"
+      :quote="data"
+      class="d-md-inline-flex justify-content-center m-5"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HomeItem from "@/components/HomeItem.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    HomeItem,
+  },
+  computed: {
+    HomeView() {
+      return this.$store.state.lists;
+    },
+  },
+  methods: {
+    getData() {
+      this.$store.dispatch("fetchNews");
+    },
+  },
+  mounted() {
+    this.getData();
   },
 };
 </script>
+
+<style scoped>
+.home {
+  justify-content: center;
+  text-align: center;
+  margin-top: 16px;
+  padding-top: 16px;
+}
+</style>
