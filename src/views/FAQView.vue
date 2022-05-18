62f12c82d0a12c7acf3904bcb756f2246d121091
raihan4816 <raihan4816@gmail.com>
2022-05-18 22:02:41 +0700

    add FAQ Page

---
+++ src/views/FAQView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="faq m-5">
+    <FAQComp />
+  </div>
+</template>
+
+<script>
+import FAQComp from "@/components/FAQComp.vue";
+
+export default {
+  name: "FAQView",
+  components: {
+    FAQComp,
+  },
+};
+</script>
+
+<style scoped></style>
