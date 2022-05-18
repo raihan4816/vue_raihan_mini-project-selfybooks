b61138986c77a0362d233275f175b7e919e170eb
raihan4816 <raihan4816@gmail.com>
2022-05-18 21:58:36 +0700

    add vue router for routing all pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,100 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import HomeLayout from "../layout/HomeLayout.vue";
+import BookLayout from "../layout/BookLayout.vue";
 import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import FAQView from "../views/FAQView.vue";
+import MotivationView from "../views/MotivationView.vue";
+import PhilosophyView from "../views/PhilosophyView.vue";
+import QuotesView from "../views/QuotesView.vue";
+import BookView from "../views/BookView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    component: HomeLayout,
+    children: [
+      {
+        path: "",
+        name: "Home",
+        component: HomeView,
+      },
+    ],
   },
   {
     path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: HomeLayout,
+    children: [
+      {
+        path: "",
+        name: "About",
+        component: AboutView,
+      },
+    ],
+  },
+  {
+    path: "/faq",
+    component: HomeLayout,
+    children: [
+      {
+        path: "",
+        name: "FAQ",
+        component: FAQView,
+      },
+    ],
+  },
+  {
+    path: "/motivation",
+    component: HomeLayout,
+    children: [
+      {
+        path: "",
+        name: "Motivation",
+        component: MotivationView,
+      },
+    ],
+  },
+  {
+    path: "/philosophy",
+    component: HomeLayout,
+    children: [
+      {
+        path: "",
+        name: "Philosophy",
+        component: PhilosophyView,
+      },
+    ],
+  },
+  {
+    path: "/quotes",
+    component: HomeLayout,
+    children: [
+      {
+        path: "",
+        name: "Quotes",
+        component: QuotesView,
+      },
+    ],
+  },
+  {
+    path: "/",
+    component: BookLayout,
+    children: [
+      {
+        path: ":title",
+        name: "BookView",
+        component: BookView,
+      },
+    ],
   },
 ];
 
 const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
   routes,
 });
 
