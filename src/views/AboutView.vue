8451130fab53c9f2d9ae51ff7bb3a27822fc07ea
raihan4816 <raihan4816@gmail.com>
2022-05-18 22:01:45 +0700

    add About Page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about m-5">
+    <AboutComp />
   </div>
 </template>
+
+<script>
+import AboutComp from "@/components/AboutComp.vue";
+
+export default {
+  name: "AboutView",
+  components: {
+    AboutComp,
+  },
+};
+</script>
+
+<style scoped></style>
